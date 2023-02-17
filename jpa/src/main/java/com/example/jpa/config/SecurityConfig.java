a069f2cd442edff2b7546afffaa7fbaad1f5be96
bo kyung <bkyungkeem@daum.net>
2023-02-17 18:22:13 +0900

    Feat: Security 추가

--- jpa/src/main/java/com/example/jpa/config/SecurityConfig.java
+++ jpa/src/main/java/com/example/jpa/config/SecurityConfig.java
@@ -9,6 +9,12 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 //@EnableWebSecurity
@@ -18,6 +24,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      * 암호화, 복호화
      * default: bcrypt
      */
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*");
+//        List<String> asd = new ArrayList<>();
+//        asd.add("http://localhost:8080");//서버에서 -> 프론트  //프론트 -> 서버
+//        asd.add("localhost:8080");
+//        configuration.setAllowCredentials(true);
+//        configuration.setAllowedOrigins(asd);
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.addExposedHeader("X-ACCESS-TOKEN");
+        configuration.addExposedHeader("X-REFRESH-TOKEN");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         // 암호화된 비밀번호를 반환
@@ -45,14 +70,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable(); //h2-console화면을 사용하기 위해 해당 옵션을 disable처리
 
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/common/login").permitAll()
+               // .antMatchers("/common/login").permitAll()
                 .antMatchers("/join").permitAll()
                 .and()
                 // 로그인기능
                 .formLogin()
-                .loginPage("/login")
+                .passwordParameter("password")
+                .usernameParameter("userid")
+//                .loginPage("/login")
                 .loginProcessingUrl("/common/login")
-                .failureUrl("/login?error")
+//                .failureUrl("/login?error")
         ;
     }
 }
